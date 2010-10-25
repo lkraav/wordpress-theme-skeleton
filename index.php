3de60beb22d575e2035762cb7df913d313fd378b
leho <leho@kraav.com>
2010-10-25 16:31:15 +0300

    lisame esialgu minima-plusist saadud action-ikoonid meta-blokile

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
 	</div>
 
 	<p class="post-footer">
-		<em>Last Updated by <?php the_author(); ?> on <a href="<?php the_permalink(); ?>" title="permanent link"><?php the_modified_time('d.m.y',display); ?></a> @ <?php the_time(); ?></em>&nbsp;
-	  |&nbsp;<img class="blogicons" src="<?php echo get_bloginfo('template_directory'); ?>/images/comment.png" alt="Comments" /><em><?php comments_popup_link(__('0 Comments'), __('1 Comment'), __('% Comments')); ?></em>
-	  <?php edit_post_link('edit', '|&nbsp;<img class="blogicons" src="'. get_bloginfo('template_directory') . '/images/edit.png" alt="Edit" /><em>', '</em>'); ?>
+		<em><?php _e('Last Updated:') ?><?php the_author(); ?><?php _e('on') ?><a href="<?php the_permalink(); ?>" title="permanent link"><?php the_modified_time('d.m.y',display); ?></a> @ <?php the_time(); ?></em>&nbsp;
+	  |&nbsp;<img class="blogicons" src="<?php echo get_bloginfo('template_directory'); ?>/img/icon-comment.png" alt="Comments" /><em><?php comments_popup_link(__('0 Comments'), __('1 Comment'), __('% Comments')); ?></em>
+	  <?php edit_post_link('edit', '|&nbsp;<img class="blogicons" src="'. get_bloginfo('template_directory') . '/img/icon-edit.png" alt="Edit" /><em>', '</em>'); ?>
 	</p>
 	<p class="post-footer">
 		<em><?php the_tags(); ?></em>
