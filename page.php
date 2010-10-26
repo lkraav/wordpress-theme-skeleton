d041b629d21514e499c51b478d42ab1ce45aed81
leho <leho@kraav.com>
2010-10-26 23:51:14 +0300

    page: blogipostituste metabloki kloon. väike vahe on datumi paigutuses, leheküljel nagu oleks loogilisem datumi asumine footeris. teine lahtine küsimus on veel kommentaaride info näitamine igal lehel ja üldse lehtedele kommentaaride lubamine.

--- page.php
+++ page.php
@@ -11,10 +11,11 @@
 	</div>
 
 	<p class="post-footer">
-		<em>Last Updated by <?php the_author(); ?> on <a href="<?php the_permalink(); ?>" title="permanent link"><?php the_modified_time('d.m.y',display); ?></a> @ <?php the_time(); ?></em>&nbsp;
-	  |&nbsp;<img class="blogicons" src="<?php echo get_bloginfo('template_directory'); ?>/img/icon-comment.png" alt="Comments" /><em><?php comments_popup_link(__('0 Comments'), __('1 Comment'), __('% Comments')); ?></em>
-	  <?php edit_post_link('edit', '|&nbsp;<img class="blogicons" src="'. get_bloginfo('template_directory') . '/img/icon-edit.png" alt="Edit" /><em>', '</em>'); ?>
+		<em><?php _e('Written by:', 'stereotunnel') ?><?php echo get_avatar(get_the_author_meta('user_email'), $size = '32'); ?><?php the_author(); ?></em>
+		<img class="blogicons" src="<?php echo get_bloginfo('template_directory'); ?>/img/icon-comment.png" alt="Comments" /><?php comments_popup_link(__('0 Comments', 'stereotunnel'), __('1 Comment', 'stereotunnel'), __('% Comments', 'stereotunnel')); ?>
+		<?php edit_post_link(__('Edit', 'stereotunnel'), '<img class="blogicons" src="'.get_bloginfo('template_directory').'/img/icon-edit.png" alt="'.__('Edit', 'stereotunnel').'" /><em>', '</em>'); ?>
 	</p>
+	<div class="post-datum"><span class="post-footer"><?php _e('Last edited:', 'stereotunnel') ?></span> <?php the_modified_time('d.m.y', display); ?>, <?php the_time('G:i'); ?></div>
 	<p class="post-footer">
 		<em><?php the_tags(); ?></em>
 	</p>	
