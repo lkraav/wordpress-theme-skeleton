b2b06e49c347ddaf9225560fc726743a67ecc7ed
leho <leho@kraav.com>
2010-10-26 23:22:11 +0300

    * gravatar tugi blog autoritele * blogposti datum eraldi väli pealkirja ja sisu vahele * blogi metaväljad tõlgitud

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
 
 	<h1 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+	<div class="post-datum"><?php the_modified_time('d.m.y', display); ?>, <?php the_time('G:i'); ?></div>
 	
 	<div class="meat">
 		<?php echo the_content(); ?>
 	</div>
 
 	<p class="post-footer">
-		<em><?php _e('Last Updated:') ?><?php the_author(); ?><?php _e('on') ?><a href="<?php the_permalink(); ?>" title="permanent link"><?php the_modified_time('d.m.y',display); ?></a> @ <?php the_time(); ?></em>&nbsp;
-	  |&nbsp;<img class="blogicons" src="<?php echo get_bloginfo('template_directory'); ?>/img/icon-comment.png" alt="Comments" /><em><?php comments_popup_link(__('0 Comments'), __('1 Comment'), __('% Comments')); ?></em>
-	  <?php edit_post_link('edit', '|&nbsp;<img class="blogicons" src="'. get_bloginfo('template_directory') . '/img/icon-edit.png" alt="Edit" /><em>', '</em>'); ?>
+		<em><?php _e('Written by:', 'stereotunnel') ?><?php echo get_avatar(get_the_author_meta('user_email'), $size = '32'); ?><?php the_author(); ?></em>
+		<img class="blogicons" src="<?php echo get_bloginfo('template_directory'); ?>/img/icon-comment.png" alt="Comments" /><?php comments_popup_link(__('0 Comments', 'stereotunnel'), __('1 Comment', 'stereotunnel'), __('% Comments', 'stereotunnel')); ?>
+		<?php edit_post_link(__('Edit', 'stereotunnel'), '<img class="blogicons" src="'.get_bloginfo('template_directory').'/img/icon-edit.png" alt="'.__('Edit', 'stereotunnel').'" /><em>', '</em>'); ?>
 	</p>
 	<p class="post-footer">
 		<em><?php the_tags(); ?></em>
