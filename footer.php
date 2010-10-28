221c093459a9650737f9b4af1c13afdd62d267c1
leho <leho@kraav.com>
2010-10-28 13:24:58 +0300

    reguleerime facebook like-boxi kõrgust

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 					'menu_class' => 'footer-nav-list',
 					'depth' => 1
 				)); ?>
-				<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Ffacebook.com%2FStereoTunnel&amp;width=292&amp;colorscheme=light&amp;connections=10&amp;stream=false&amp;header=true&amp;height=260" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:260px;" allowTransparency="true"></iframe>
+				<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Ffacebook.com%2FStereoTunnel&amp;width=292&amp;colorscheme=light&amp;connections=10&amp;stream=false&amp;header=true&amp;height=290" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:290px;" allowTransparency="true"></iframe>
 			</div>
 			<?php // Footer ?>
 		</div>
