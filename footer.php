0c595e709c4b53443825876ecfd2e7bce39c1f32
leho <leho@kraav.com>
2010-11-17 16:59:38 +0200

    Revert "footer: liigume ühele footernav-ile, rohkem pole praegusel hinnangul", paar laiuste paikapanekut footer-navile + copyright tekst
    This reverts commit beb3c5f714045277a300dac2634a36b25205e3e3.

--- footer.php
+++ footer.php
@@ -7,9 +7,22 @@
 					'menu_class' => 'footer-nav',
 					'depth' => 1
 				)); ?>
+				<?php wp_nav_menu(array(
+					'container' => '',
+					'menu' => 'FooterNav2',
+					'menu_class' => 'footer-nav',
+					'depth' => 1
+				)); ?>
+				<?php wp_nav_menu(array(
+					'container' => '',
+					'menu' => 'FooterNav3',
+					'menu_class' => 'footer-nav',
+					'depth' => 1
+				)); ?>
 			</div>
 			<?php get_sidebar(); ?>
 		</div>
+		<p class="copyright">(c) MTÜ Stereo-tunnel 2010</p>
 	</div> <!-- container -->
 	<div class="hide">
 	<?php wp_footer(); ?>
